8849f73f72f79b7dc9f55bf34e7a27d7e680fe59
Alexis Franco <ronaldofranco196@gmail.com>
2022-10-17 20:54:46 -0300

    Trabajando con Set

---
+++ Set.js
@@ -0,0 +1,8 @@
+const familia = new Set(['Alexis', 'Fanny', 'Patricia']);
+console.log(familia);
+
+familia.add('Alexis');
+console.log(familia);
+
+familia.add('JavaScript');
+console.log(familia);
